e7a5a6217dbe13e0364257ea447b12ebffb130d6
rafi612 <rafi612p@outlook.com>
2024-07-30 22:22:20 +0200

    Improve grade cards

--- ui-logic/src/main/java/io/github/vulka/ui/screens/dashboard/GradesScreen.kt
+++ ui-logic/src/main/java/io/github/vulka/ui/screens/dashboard/GradesScreen.kt
@@ -13,10 +13,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.ArrowDropUp
+import androidx.compose.material.icons.filled.Book
+import androidx.compose.material.icons.filled.CalendarMonth
 import androidx.compose.material.icons.filled.Looks6
+import androidx.compose.material.icons.filled.Scale
+import androidx.compose.material.icons.filled.TextFormat
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -31,6 +37,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.produceState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -46,6 +53,10 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import dev.medzik.android.compose.rememberMutable
 import dev.medzik.android.compose.ui.ExpandedIfNotEmpty
 import dev.medzik.android.compose.ui.IconBox
+import dev.medzik.android.compose.ui.bottomsheet.BaseBottomSheet
+import dev.medzik.android.compose.ui.bottomsheet.rememberBottomSheetState
+import dev.medzik.android.compose.ui.textfield.AnimatedTextField
+import dev.medzik.android.compose.ui.textfield.TextFieldValue
 import dev.medzik.android.utils.runOnUiThread
 import io.github.vulka.core.api.Platform
 import io.github.vulka.core.api.types.Grade
@@ -57,6 +68,7 @@ import io.github.vulka.ui.common.EmptyViewProgress
 import io.github.vulka.ui.common.SegmentedButtonItem
 import io.github.vulka.ui.common.SegmentedButtons
 import io.github.vulka.ui.utils.formatByLocale
+import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -146,102 +158,68 @@ fun GradesTab(
     val semesters by viewModel.semesters.collectAsStateWithLifecycle()
     val semester by viewModel.semester.collectAsStateWithLifecycle()
 
-    if (semesters.isNotEmpty() && semester != null) {
-        val gradeList by viewModel.gradesList.collectAsStateWithLifecycle()
-        val uniqueSubjectNames by viewModel.uniqueSubjectNames.collectAsStateWithLifecycle()
+    Box(
+        modifier = Modifier.fillMaxSize()
+            .nestedScroll(connection = pullToRefreshState.nestedScrollConnection)
+    ) {
+        if (semesters.isNotEmpty() && semester != null) {
+            val gradeList by viewModel.gradesList.collectAsStateWithLifecycle()
+            val uniqueSubjectNames by viewModel.uniqueSubjectNames.collectAsStateWithLifecycle()
 
-        Box(
-            modifier = Modifier.nestedScroll(connection = pullToRefreshState.nestedScrollConnection)
-        ) {
             if (gradeList.isNotEmpty()) {
-                LazyColumn(
-                    modifier = Modifier.fillMaxSize()
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .verticalScroll(rememberScrollState()),
                 ) {
-                    item {
-                        Row(
-                            modifier = Modifier
-                                .padding(horizontal = 50.dp)
-                                .padding(top = 10.dp)
-                                .fillMaxWidth()
-                                .height(35.dp),
-                            horizontalArrangement = Arrangement.Center
-                        ) {
-                            SegmentedButtons {
-                                for (s in semesters) {
-                                    SegmentedButtonItem(
-                                        selected = semester!!.semester.number == s.semester.number,
-                                        onClick = {
-                                            viewModel.setSemester(s)
-                                            viewModel.refreshGrades(args)
-                                        },
-                                        label = { Text("${stringResource(R.string.Semester)} ${s.semester.number}") }
-                                    )
-                                }
+                    Row(
+                        modifier = Modifier
+                            .padding(horizontal = 50.dp)
+                            .padding(top = 10.dp)
+                            .fillMaxWidth()
+                            .height(35.dp),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        SegmentedButtons {
+                            for (s in semesters) {
+                                SegmentedButtonItem(
+                                    selected = semester!!.semester.number == s.semester.number,
+                                    onClick = {
+                                        viewModel.setSemester(s)
+                                        viewModel.refreshGrades(args)
+                                    },
+                                    label = {
+                                        Text("${stringResource(R.string.Semester)} ${s.semester.number}")
+                                    }
+                                )
                             }
                         }
                     }
-                    item {
-                        uniqueSubjectNames.forEach { subjectName ->
-                            SubjectCard(
-                                more = {
-                                    val filterGrades =
-                                        gradeList.filter { it.subject == subjectName }
-
-                                    filterGrades.forEach { grade ->
-                                        Row(
-                                            modifier = Modifier
-                                                .fillMaxWidth()
-                                                .heightIn(min = 32.dp)
-                                                .padding(vertical = 2.dp),
-                                        ) {
-                                            val gradeColor = GradeColor.getColorByGrade(grade)
-                                            Avatar(
-                                                text = grade.value.orEmpty(),
-                                                shape = AvatarShape.Rounded,
-                                                cardColors = if (gradeColor != null) {
-                                                    CardDefaults.cardColors()
-                                                        .copy(
-                                                            containerColor = gradeColor.containerColor,
-                                                            contentColor = gradeColor.contentColor
-                                                        )
-                                                } else CardDefaults.cardColors()
-                                            )
-
-                                            Column(
-                                                Modifier
-                                                    .fillMaxWidth()
-                                                    .weight(1f)
-                                                    .padding(horizontal = 10.dp)
-                                            ) {
-                                                Text(
-                                                    text = grade.name,
-                                                    maxLines = 1,
-                                                    overflow = TextOverflow.Ellipsis
-                                                )
-                                                Text(
-                                                    fontSize = 12.sp,
-                                                    text = "${grade.date.formatByLocale(Locale.current)}  ${stringResource(R.string.Weight)}: ${grade.weight}"
-                                                )
-                                            }
-                                        }
-                                    }
+                    uniqueSubjectNames.forEach { subjectName ->
+                        SubjectCard(
+                            more = {
+                                gradeList
+                                    .filter { it.subject == subjectName }
+                                    .sortedByDescending { it.date }
+                                    .forEach { grade ->
+                                    GradeCard(grade = grade)
                                 }
-                            ) {
-                                Text(subjectName)
-
-                                val gradesAmount by produceState(initialValue = 0, subjectName, semester) {
-                                    viewModel.countGrades(
-                                        userId = args.userId,
-                                        semester = semester!!.semester.number,
-                                        subjectName = subjectName
-                                    ).collect { value = it }
-                                }
-
-                                Text(
-                                    fontSize = 12.sp,
-                                    text = "$gradesAmount ${pluralStringResource(R.plurals.GradesAmount,gradesAmount)}"
-                                )
                             }
+                        ) {
+                            Text(subjectName)
+
+                            val gradesAmount by produceState(initialValue = 0, subjectName, semester) {
+                                viewModel.countGrades(
+                                    userId = args.userId,
+                                    semester = semester!!.semester.number,
+                                    subjectName = subjectName
+                                ).collect { value = it }
+                            }
+
+                            Text(
+                                fontSize = 12.sp,
+                                text = "$gradesAmount ${pluralStringResource(R.plurals.GradesAmount,gradesAmount)}"
+                            )
                         }
                     }
                 }
@@ -251,8 +229,8 @@ fun GradesTab(
                     title = stringResource(R.string.Grades_Empty)
                 )
             }
-        }
-    } else EmptyViewProgress()
+        } else EmptyViewProgress()
+    }
 }
 
 @Composable
@@ -263,14 +241,14 @@ fun SubjectCard(
     var showMore by rememberMutable { false }
 
     Surface(
-        onClick = {
-            showMore = !showMore
-        },
         modifier = Modifier
             .fillMaxWidth()
             .padding(horizontal = 8.dp, vertical = 12.dp),
         shape = MaterialTheme.shapes.medium,
-        color = MaterialTheme.colorScheme.surfaceContainer
+        color = MaterialTheme.colorScheme.surfaceContainer,
+        onClick = {
+            showMore = !showMore
+        }
     ) {
         Column(
             modifier = Modifier.fillMaxSize()
@@ -296,12 +274,154 @@ fun SubjectCard(
 
             ExpandedIfNotEmpty(Unit.takeIf { showMore }) {
                 Column(
-                    modifier = Modifier.padding(12.dp),
+                    modifier = Modifier.padding(6.dp),
                 ) {
                     more()
                 }
             }
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun GradeCard(grade: Grade) {
+    val bottomSheetState = rememberBottomSheetState()
+    val scope = rememberCoroutineScope()
+
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth(),
+        shape = MaterialTheme.shapes.medium,
+        color = Color.Transparent,
+        onClick = {
+            bottomSheetState.show()
+        }
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .heightIn(min = 32.dp)
+                .padding(3.dp)
+        ) {
+            val gradeColor = GradeColor.getColorByGrade(grade)
+            Avatar(
+                text = grade.value.orEmpty(),
+                shape = AvatarShape.Rounded,
+                cardColors = if (gradeColor != null) {
+                    CardDefaults.cardColors()
+                        .copy(
+                            containerColor = gradeColor.containerColor,
+                            contentColor = gradeColor.contentColor
+                        )
+                } else CardDefaults.cardColors()
+            )
+
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                    .padding(horizontal = 10.dp)
+            ) {
+                Text(
+                    text = grade.name,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(20.dp)
+                ) {
+                    Text(
+                        fontSize = 12.sp,
+                        text = grade.date.formatByLocale(Locale.current)
+                    )
+                    Text(
+                        fontSize = 12.sp,
+                        text = "${stringResource(R.string.Weight)}: ${grade.weight}"
+                    )
+                }
+            }
+
+        }
+    }
+    BaseBottomSheet(
+        state = bottomSheetState,
+        onDismiss = {
+            scope.launch {
+                bottomSheetState.hide()
+            }
+        }
+    ) {
+        GradesDetails(grade)
+    }
+}
+
+@Composable
+private fun GradesDetails(grade: Grade) {
+    Column(
+        modifier = Modifier.padding(horizontal = 12.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        if (grade.name.isNotEmpty()) {
+            AnimatedTextField(
+                value = TextFieldValue(
+                    value = grade.name,
+                    editable = false
+                ),
+                label = stringResource(R.string.Name),
+                leading = {
+                    IconBox(Icons.Default.TextFormat)
+                }
+            )
+        }
+
+        AnimatedTextField(
+            value = TextFieldValue(
+                value = grade.subject,
+                editable = false
+            ),
+            label = stringResource(R.string.Subject),
+            leading = {
+                IconBox(Icons.Default.Book)
+            }
+        )
+
+        AnimatedTextField(
+            value = TextFieldValue(
+                value = grade.value.orEmpty(),
+                editable = false
+            ),
+            label = stringResource(R.string.Grades),
+            leading = {
+                IconBox(Icons.Default.Looks6)
+            }
+        )
+
+        AnimatedTextField(
+            value = TextFieldValue(
+                value = grade.weight.toString(),
+                editable = false
+            ),
+            label = stringResource(R.string.Weight),
+            leading = {
+                IconBox(Icons.Default.Scale)
+            }
+        )
+
+        AnimatedTextField(
+            value = TextFieldValue(
+                value = grade.date.formatByLocale(Locale.current),
+                editable = false
+            ),
+            label = stringResource(R.string.Date),
+            leading = {
+                IconBox(Icons.Default.CalendarMonth)
+            }
+        )
+
     }
 }
 
